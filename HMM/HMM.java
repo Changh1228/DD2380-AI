be4a1aa25afdd7b28d6da55add5b708073aaae63
Changh1228 <43349134+Changh1228@users.noreply.github.com>
2019-02-01 20:52:25 +0100

    Add files via upload

---
+++ HMM/HMM.java
@@ -0,0 +1,272 @@
+/*
+ * @Author: Hongsheng Chang
+ * @Date: 2019-01-25 08:12:54
+ * @Last Modified by:   Hongsheng Chang
+ * @Last Modified time: 2019-01-25 08:12:54
+ */
+import java.util.*;
+public class HMM {
+
+    private int N = 6;
+    private int K = 8;
+    private int T;
+    private double[] c;
+    private int[] Obs;
+    private double[][] Alpha, Beta, Gamma;
+    private double[][][] DiGamma;
+
+    public int predict = 0;
+    public double[] pi ;//= {0.241896, 0.266086, 0.249153, 0.242864};
+    public double[][] A;// = {{0.4, 0.2, 0.2, 0.2},{0.2, 0.4, 0.2, 0.2}, {0.2, 0.2, 0.4, 0.2}, {0.2, 0.2, 0.2, 0.4}};
+    public double[][] B;// = {{0.4, 0.2, 0.2, 0.2}, {0.2, 0.4, 0.2, 0.2}, {0.2, 0.2, 0.4, 0.2}, {0.2, 0.2, 0.2, 0.4}};
+
+    public HMM(int[] Obs){
+        this.Obs = Obs;
+        T = Obs.length;
+        pi = new double[N];
+        double rand = 0.01*Math.random();
+        for (int i = 0; i < N; ++i) {
+            pi[i] = (1.0/ N +rand);
+            rand = -rand;
+        }
+
+        A = new double[N][N];
+        rand = 0.01*Math.random();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = (1.0 / N + rand);
+                rand = -rand;
+            }
+        }
+
+        B = new double[N][K];
+        for (int i = 0; i < N; i++) {
+            rand = 0.01*Math.random();
+            for (int j = 0; j < K; j++) {
+                B[i][j] = (1.0 / K + rand);
+                rand = -rand;
+                if (j == 7) {
+                    rand = 0;
+                }
+            }
+        }
+
+        Alpha = new double[T][N];
+        Beta = new double[T][N];
+        Gamma = new double[T][N];
+        DiGamma = new double[T][N][N];
+        c = new double[T];
+    }
+
+    private void Print_Matrix_2D(double[][]Matrix){
+        int X = Matrix.length;
+        int Y = Matrix[0].length;
+
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < Y; j++) {
+                System.out.print(Matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    private void Alpha_pass(){
+        int k = Obs[0]; // first step t=1
+        c[0] = 0;
+        for (int i = 0; i < N; i++) {
+            Alpha[0][i] = B[i][k] * pi[i];
+            c[0] += Alpha[0][i];
+        }
+        c[0] = 1.0 / c[0];
+        //System.out.println("c ");
+        //System.out.print(c[0] + " ");
+        for (int i = 0; i < N; i++) { // scale Alpha 0
+            Alpha[0][i] *= c[0];
+        }
+
+        for (int t = 1; t < T; t++) { // t timestep
+            c[t] = 0;
+            k = Obs[t];
+            for (int i = 0; i < N; i++) {
+                Alpha[t][i] = 0;
+                for (int j = 0; j < N; j++) {
+                    Alpha[t][i] += ( Alpha[t-1][j] * A[j][i] * B[i][k] );
+                }
+                c[t] += Alpha[t][i];
+            }
+            c[t] = 1.0 / c[t];
+            //System.out.print(c[t] + " ");
+
+            for (int i = 0; i < N; i++) {
+                Alpha[t][i] *= c[t];
+            }
+
+        }
+        /*System.out.println();
+        System.out.println("Alpha");
+        Print_Matrix_2D(Alpha);
+        System.out.println();*/
+    }
+
+    private void Beta_pass(){
+        for (int i = 0; i < N; i++) { // T timestep
+            Beta[T-1][i] = 1.0 / c[T-1];
+        }
+
+        for (int t = T-2; t >= 0; t--) { // t timestep
+            int k = Obs[t+1];
+            for (int i = 0; i < N; i++) {
+                Beta[t][i] = 0;
+                for (int j = 0; j < N; j++) {
+                    Beta[t][i] += ( A[i][j] * B[j][k] * Beta[t+1][j]);
+                }
+                Beta[t][i] *= c[t];
+            }
+        }
+        /*System.out.println("Beta");
+        Print_Matrix_2D(Beta);
+        System.out.println();*/
+    }
+
+    private void Cal_Gammas(){
+        for (int t = 0; t < T-1; t++) { // Gammma t
+            double denom = 0;
+            int k = Obs[t+1];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    denom += ( Alpha[t][i] * A[i][j] * B[j][k] * Beta[t+1][j] );
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                Gamma[t][i] = 0;
+                for (int j = 0; j < N; j++) {
+                    DiGamma[t][i][j] = (Alpha[t][i] * A[i][j] * B[j][k] *
+                                        Beta[t+1][j] ) / denom;
+                    Gamma[t][i] += DiGamma[t][i][j];
+                }
+            }
+        }
+
+        double denom = 0; // special case for Gamma T-1
+        for (int i = 0; i < N; i++) {
+            denom += Alpha[T-1][i];
+        }
+        for (int i = 0; i < N; i++) {
+            Gamma[T-1][i] = Alpha[T-1][i] / denom;
+        }
+        /*System.out.println("Gamma");
+        Print_Matrix_2D(Gamma);
+        System.out.println();
+
+        System.out.println("DiGamma");
+        for (int i= 0; i < 4 ; i++) {
+            System.out.println("t="+i);
+            Print_Matrix_2D(DiGamma[i]);
+        }*/
+    }
+
+    private void Cal_para(){
+        //System.out.println("pi");
+        for (int i = 0; i < N; i++) { // estimate pi
+            pi[i] = Gamma[0][i];
+            //System.out.print(pi[i]+ " ");
+        }
+        //System.out.println();
+        for (int i = 0; i < N; i++) { // estimate A
+            for (int j = 0; j < N; j++) {
+                double number = 0;
+                double denom = 0;
+                for (int t = 0; t < T-1; t++) {
+                    number += DiGamma[t][i][j];
+                    denom += Gamma[t][i];
+                }
+                A[i][j] = number / denom;
+            }
+        }
+        /*System.out.println("A");
+        Print_Matrix_2D(A);*/
+        for (int i = 0; i < N; i++) { //estimate B
+            for (int j = 0; j < K; j++) {
+                double number = 0;
+                double denom = 0;
+                for (int t = 0; t < T-1; t++){
+                    if (Obs[t] == j) {
+                        number += Gamma[t][i];
+                    }
+                    denom += Gamma[t][i];
+                }
+                B[i][j] = number/denom;
+            }
+        }
+        /*System.out.println("B");
+        Print_Matrix_2D(B);*/
+    }
+
+    public void BaumWelch(int[] Obs){ // run prediction
+        //Init(double[] Obs);
+        double oldlogProb = Integer.MIN_VALUE;
+        double logProb = Integer.MIN_VALUE + 1;
+        int q = 0;
+        for (int it = 0; it < 999999 && logProb > oldlogProb; it++) {
+            //System.out.println("oldlogProb" + oldlogProb +" "+ logProb);
+            Alpha_pass();
+            Beta_pass();
+            Cal_Gammas();
+            Cal_para();
+
+            oldlogProb = logProb; // cal logProb
+            logProb = 0;
+            for (int i = 0; i < T; i++) {
+                logProb += Math.log(c[i]);
+            }
+            logProb = -logProb;
+            q = it;
+        }
+        System.out.println(q);
+
+    }
+    public int predict_next_Obs(){
+        // predict next action
+        double[] buff = pi;
+        for (int i=0; i<N; i++) {
+            System.out.print(pi[i] + " ");
+        }
+        System.out.println();
+        System.out.println("A");
+        Print_Matrix_2D(A);
+        double[] pi_t = new double[N];
+
+        for (int t = 0; t < T; t++) {
+            for (int i=0; i<N; i++) {
+                pi_t[i]=0;// init pit
+            }
+            System.out.println(" Pit=" + t);
+            for (int i = 0; i < N; i++) { // Cal hidden state
+                for (int j = 0; j < N; j++) {
+                    pi_t[i] += ( buff[j] * A[j][i] );
+                }
+                System.out.print(pi_t[i]+ " ");
+            }
+            System.out.println(" ");
+            buff = Arrays.copyOf(pi_t,N);
+
+        }
+        double[] Obs_t = new double[K];
+        double max = 0;
+        for (int i = 0; i < K; i++) { // Cal final Obs prob
+            Obs_t[i] = 0;
+            for (int j = 0; j < N; j++) {
+                Obs_t[i] += ( pi_t[j] * B[j][i] );
+            }
+            if (Obs_t[i] > max) { // find max index of Obs_t
+                max = Obs_t[i];
+                predict = i;
+            }
+        }
+        if (max < 0.3 ) {
+            predict = 10; // not sure don't shoot
+        }
+        System.out.println(" ");
+        System.out.println(max);
+        return predict;
+    }
+}
